edb0e0155dbb02011165e72c0ab17550a40ecf31
Roel Huybrechts <roel@huybrechts.re>
2015-05-05 13:41:41 +0200

    Update, annotate.

--- annotate.py
+++ annotate.py
@@ -5,29 +5,54 @@ from qgis.core import *
 
 class AnnotationManager(object):
     def __init__(self, main):
+        def drawArrow(start):
+            if start:
+                if self.currentlyEditing:
+                    QObject.disconnect(self.main.iface.mapCanvas(), SIGNAL('mapToolSet(QgsMapTool*)'), self.mapToolChanged)
+                    self.main.utils.stopEditInLayer(self.currentlyEditing[0])
+                    self.currentlyEditing[1].setChecked(False)
+                self.currentlyEditing = ('Pijlen', self.annotateArrow)
+                self.main.utils.editInLayer('Pijlen')
+                QObject.connect(self.main.iface.mapCanvas(), SIGNAL('mapToolSet(QgsMapTool*)'), self.mapToolChanged)
+            else:
+                QObject.disconnect(self.main.iface.mapCanvas(), SIGNAL('mapToolSet(QgsMapTool*)'), self.mapToolChanged)
+                if self.currentlyEditing:
+                    self.main.utils.stopEditInLayer(self.currentlyEditing[0])
+                self.currentlyEditing = None
+
+        def drawPolygon(start):
+            if start:
+                if self.currentlyEditing:
+                    QObject.disconnect(self.main.iface.mapCanvas(), SIGNAL('mapToolSet(QgsMapTool*)'), self.mapToolChanged)
+                    self.main.utils.stopEditInLayer(self.currentlyEditing[0])
+                    self.currentlyEditing[1].setChecked(False)
+                self.currentlyEditing = ('Polygonen', self.annotatePolygon)
+                self.main.utils.editInLayer('Polygonen')
+                QObject.connect(self.main.iface.mapCanvas(), SIGNAL('mapToolSet(QgsMapTool*)'), self.mapToolChanged)
+            else:
+                QObject.disconnect(self.main.iface.mapCanvas(), SIGNAL('mapToolSet(QgsMapTool*)'), self.mapToolChanged)
+                if self.currentlyEditing:
+                    self.main.utils.stopEditInLayer(self.currentlyEditing[0])
+                self.currentlyEditing = None
+
         self.main = main
-        #QObject.connect(self.mapCanvas, SIGNAL('mapToolSet(QgsMapTool*)'), self.mapToolChanged)
+        self.currentlyEditing = None
 
         self.annotateArrow = QAction(QIcon(':/icons/icons/pijlen.png'), 'Teken een pijl', self.main.iface)
         self.annotateArrow.setCheckable(True)
-        QObject.connect(self.annotateArrow, SIGNAL('triggered(bool)'), self.drawArrow)
+        QObject.connect(self.annotateArrow, SIGNAL('triggered(bool)'),  drawArrow)
 
         self.annotatePolygon = QAction(QIcon(':/icons/icons/polygonen.png'), 'Teken een polygoon', self.main.iface)
         self.annotatePolygon.setCheckable(True)
-        QObject.connect(self.annotatePolygon, SIGNAL('triggered(bool)'), self.drawPolygon)
+        QObject.connect(self.annotatePolygon, SIGNAL('triggered(bool)'), drawPolygon)
 
     def addActionsToToolbar(self, toolbar):
         toolbar.addAction(self.annotateArrow)
         toolbar.addAction(self.annotatePolygon)
-  
-    def drawArrow(self, start):
-        if start:
-            self.main.utils.editInLayer('Pijlen')
-        else:
-            self.main.utils.stopEditInLayer('Pijlen')
 
-    def drawPolygon(self, start):
-        if start:
-            self.main.utils.editInLayer('Polygonen')
-        else:
-            self.main.utils.stopEditInLayer('Polygonen')
+    def mapToolChanged(self, mt):
+        QObject.disconnect(self.main.iface.mapCanvas(), SIGNAL('mapToolSet(QgsMapTool*)'), self.mapToolChanged)
+        if self.currentlyEditing:
+            self.main.utils.stopEditInLayer(self.currentlyEditing[0])
+            self.currentlyEditing[1].setChecked(False)
+            self.currentlyEditing = None
