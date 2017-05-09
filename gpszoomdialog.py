cb021055cb6cad528bf4986666652e80b22905e9
Roel Huybrechts <roel@huybrechts.re>
2017-05-09 14:44:34 +0200

    Zoom naar GPS ook in decimale graden.

--- gpszoomdialog.py
+++ gpszoomdialog.py
@@ -5,6 +5,22 @@ from qgis.core import *
 
 from ui_gpszoomdialog import Ui_GpsZoomDialog
 
+class RoundingDoubleValidator(QDoubleValidator):
+    def __init__(self, lineEdit, bottom, top, decimals):
+        self.lineEdit = lineEdit
+        QDoubleValidator.__init__(self, bottom, top, decimals)
+
+    def fixup(self, value):
+        try:
+            v = float(value)
+            if v < self.bottom():
+                value = str(self.bottom())
+            if v > self.top():
+                value = str(self.top())
+        except ValueError:
+            value = '0'
+
+        self.lineEdit.setText(value)
 
 class GpsZoomDialog(QDialog, Ui_GpsZoomDialog):
     def __init__(self, main):
@@ -12,37 +28,86 @@ class GpsZoomDialog(QDialog, Ui_GpsZoomDialog):
         QDialog.__init__(self, self.main.iface.mainWindow())
         self.setupUi(self)
 
+        self.led_latDecDeg.setValidator(RoundingDoubleValidator(self.led_latDecDeg, -90.0, 90.0, 10))
+        self.led_lonDecDeg.setValidator(RoundingDoubleValidator(self.led_lonDecDeg, -180.0, 180.0, 10))
+
+        self.transform_31370_to_4326 = QgsCoordinateTransform(
+            QgsCoordinateReferenceSystem(31370, QgsCoordinateReferenceSystem.EpsgCrsId),
+            QgsCoordinateReferenceSystem(4326, QgsCoordinateReferenceSystem.EpsgCrsId))
+
+        self.transform_4326_to_31370 = QgsCoordinateTransform(
+            QgsCoordinateReferenceSystem(4326, QgsCoordinateReferenceSystem.EpsgCrsId),
+            QgsCoordinateReferenceSystem(31370, QgsCoordinateReferenceSystem.EpsgCrsId))
+
+        point = self.main.iface.mapCanvas().extent().center()
+        self.point = self.transform_31370_to_4326.transform(point)
+
+        self.connectChangedSignals()
+
         QObject.connect(self.btn_zoom, SIGNAL('clicked(bool)'), self.zoomTo)
         QObject.connect(self.btn_zoom, SIGNAL('clicked(bool)'), self.hide)
 
         self.populate()
 
-    def populate(self):
-        center = self.main.iface.mapCanvas().extent().center()
-        t = QgsCoordinateTransform(QgsCoordinateReferenceSystem(31370, QgsCoordinateReferenceSystem.EpsgCrsId),
-            QgsCoordinateReferenceSystem(4326, QgsCoordinateReferenceSystem.EpsgCrsId))
-        center = t.transform(center)
+    def connectChangedSignals(self):
+        for w in (self.sb_latDeg, self.sb_latMin, self.sb_latSec, self.sb_lonDeg, self.sb_lonMin, self.sb_lonSec):
+            QObject.connect(w, SIGNAL('valueChanged(QString)'), self.updatePoint_dms)
 
-        latDeg = int(center.y())
-        latMin = abs(center.y()-latDeg)*60.0
+        for w in (self.led_latDecDeg, self.led_lonDecDeg):
+            QObject.connect(w, SIGNAL('textChanged(QString)'), self.updatePoint_deg)
+
+    def disconnectChangedSignals(self):
+        for w in (self.sb_latDeg, self.sb_latMin, self.sb_latSec, self.sb_lonDeg, self.sb_lonMin, self.sb_lonSec):
+            QObject.disconnect(w, SIGNAL('valueChanged(QString)'), self.updatePoint_dms)
+
+        for w in (self.led_latDecDeg, self.led_lonDecDeg):
+            QObject.disconnect(w, SIGNAL('textChanged(QString)'), self.updatePoint_deg)
+
+    def populate(self, disable=None):
+        self.disconnectChangedSignals()
+
+        latDeg = int(self.point.y())
+        latMin = abs(self.point.y()-latDeg)*60.0
         latSec = abs(latMin-int(latMin))*60.0
-        self.sb_latDeg.setValue(latDeg)
-        self.sb_latMin.setValue(int(latMin))
-        self.sb_latSec.setValue(latSec)
 
-        lonDeg = int(center.x())
-        lonMin = abs(center.x()-lonDeg)*60.0
+        if not disable == 'dms':
+            self.sb_latDeg.setValue(latDeg)
+            self.sb_latMin.setValue(int(latMin))
+            self.sb_latSec.setValue(latSec)
+        if not disable == 'deg':
+            self.led_latDecDeg.setText('%f' % self.point.y())
+
+        lonDeg = int(self.point.x())
+        lonMin = abs(self.point.x()-lonDeg)*60.0
         lonSec = abs(lonMin-int(lonMin))*60.0
-        self.sb_lonDeg.setValue(lonDeg)
-        self.sb_lonMin.setValue(int(lonMin))
-        self.sb_lonSec.setValue(lonSec)
 
-    def zoomTo(self):
+        if not disable == 'dms':
+            self.sb_lonDeg.setValue(lonDeg)
+            self.sb_lonMin.setValue(int(lonMin))
+            self.sb_lonSec.setValue(lonSec)
+        if not disable == 'deg':
+            self.led_lonDecDeg.setText('%f' % self.point.x())
+
+        self.connectChangedSignals()
+
+    def updatePoint_dms(self):
         lat = self.sb_latDeg.value() + (self.sb_latMin.value()/60.0) + (self.sb_latSec.value()/6000.0)
         lon = self.sb_lonDeg.value() + (self.sb_lonMin.value()/60.0) + (self.sb_lonSec.value()/6000.0)
-        t = QgsCoordinateTransform(QgsCoordinateReferenceSystem(4326, QgsCoordinateReferenceSystem.EpsgCrsId),
-            QgsCoordinateReferenceSystem(31370, QgsCoordinateReferenceSystem.EpsgCrsId))
-        p = t.transform(lon, lat)
+        self.point.setY(lat)
+        self.point.setX(lon)
+
+        self.populate(disable='dms')
+
+    def updatePoint_deg(self):
+        lat = float(self.led_latDecDeg.text())
+        lon = float(self.led_lonDecDeg.text())
+        self.point.setY(lat)
+        self.point.setX(lon)
+
+        self.populate(disable='deg')
+
+    def zoomTo(self):
+        p = self.transform_4326_to_31370.transform(self.point)
 
         self.main.iface.mapCanvas().zoomScale(5000)
         self.main.iface.mapCanvas().zoomByFactor(1, p)
