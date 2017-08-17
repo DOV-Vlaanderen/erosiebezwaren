b327d003eb442248ef70ce1b226e205492d74489
Roel Huybrechts <roel@huybrechts.re>
2017-08-17 11:31:46 +0200

    Add GPLv2 license.

--- erosiebezwarenwidget.py
+++ erosiebezwarenwidget.py
@@ -1,3 +1,25 @@
+#-*- coding: utf-8 -*-
+
+#  DOV Erosiebezwaren, QGis plugin to assess field erosion on tablets
+#  Copyright (C) 2015-2017  Roel Huybrechts
+#
+#  Partly based on the loop visible layers plugin (https://github.com/etiennesky/loopvisiblelayers)
+#  Copyright (C) 2012 Etienne Tourigny
+#
+#  This program is free software; you can redistribute it and/or modify
+#  it under the terms of the GNU General Public License as published by
+#  the Free Software Foundation; either version 2 of the License, or
+#  (at your option) any later version.
+#
+#  This program is distributed in the hope that it will be useful,
+#  but WITHOUT ANY WARRANTY; without even the implied warranty of
+#  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+#  GNU General Public License for more details.
+#
+#  You should have received a copy of the GNU General Public License along
+#  with this program; if not, write to the Free Software Foundation, Inc.,
+#  51 Franklin Street, Fifth Floor, Boston, MA 02110-1301 USA.
+
 from PyQt4.QtCore import *
 from PyQt4 import QtGui
 from PyQt4.QtGui import QIcon, QPixmap
